447174ae6bf4d66140694cd2b3452471cdf29bb5
Supriya Kalghatgi <supriya@geekyants.com>
2018-12-18 17:58:54 +0530

    changed to RN app, added AntDesign - FontAwesome5 - changed IonIcons

--- src/components/IconFamilyList.js
+++ src/components/IconFamilyList.js
@@ -1,68 +1,74 @@
 import React, { Component } from 'react';
-import Expo from "expo";
+// import Expo from "expo";
 import { Container, Header, Content, Left, Body, Right, Button, Title, List, ListItem, Text } from 'native-base';
 import IconList from './IconList';
 
 export default class IconFamilyList extends Component {
 
-    state = { loading: true }
+  state = { loading: true }
 
-    async componentWillMount() {
-        await Expo.Font.loadAsync({
-            Roboto: require("native-base/Fonts/Roboto.ttf"),
-            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-            Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-        });
-        this.setState({ loading: false });
-    }
+  // async componentWillMount() {
+  //     await Expo.Font.loadAsync({
+  //         Roboto: require("native-base/Fonts/Roboto.ttf"),
+  //         Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+  //         Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+  //     });
+  //     this.setState({ loading: false });
+  // }
 
-    render() {
-        if (this.state.loading) {
-            return <Expo.AppLoading />;
-        }
-        return (
-            <Container>
-                <Header>
-                    <Body><Title>Icon Families</Title></Body>
-                </Header>
-                <Content>
-                    <List>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Entypo" })}>
-                            <Text>Entypo</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "EvilIcons" })}>
-                            <Text>EvilIcons</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Feather" })}>
-                            <Text>Feather</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "FontAwesome" })}>
-                            <Text>FontAwesome</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Foundation" })}>
-                            <Text>Foundation</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Ionicons" })}>
-                            <Text>Ionicons</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "MaterialIcons" })}>
-                            <Text>MaterialIcons</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "MaterialCommunityIcons" })}>
-                            <Text>MaterialCommunityIcons</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Octicons" })}>
-                            <Text>Octicons</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "SimpleLineIcons" })}>
-                            <Text>SimpleLineIcons</Text>
-                        </ListItem>
-                        <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Zocial" })}>
-                            <Text>Zocial</Text>
-                        </ListItem>
-                    </List>
-                </Content>
-            </Container>
-        );
-    }
+  render() {
+    // if (this.state.loading) {
+    //     return <Expo.AppLoading />;
+    // }
+    return (
+      <Container>
+        <Header>
+          <Body><Title>Icon Families</Title></Body>
+        </Header>
+        <Content>
+          <List>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "AntDesign" })}>
+              <Text>AntDesign</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Entypo" })}>
+              <Text>Entypo</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "EvilIcons" })}>
+              <Text>EvilIcons</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Feather" })}>
+              <Text>Feather</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "FontAwesome" })}>
+              <Text>FontAwesome</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "FontAwesome5" })}>
+              <Text>FontAwesome5</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Foundation" })}>
+              <Text>Foundation</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Ionicons" })}>
+              <Text>Ionicons</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "MaterialIcons" })}>
+              <Text>MaterialIcons</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "MaterialCommunityIcons" })}>
+              <Text>MaterialCommunityIcons</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Octicons" })}>
+              <Text>Octicons</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "SimpleLineIcons" })}>
+              <Text>SimpleLineIcons</Text>
+            </ListItem>
+            <ListItem onPress={() => this.props.navigation.navigate('IconList', { iconFamily: "Zocial" })}>
+              <Text>Zocial</Text>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
+  }
 }
--- src/components/IconList.js
+++ src/components/IconList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, FlatList } from 'react-native'
-import Expo from "expo";
+// import Expo from "expo";
 import { Container, Header, Content, View, Icon, Left, Button, Body, Right, Title, Text } from 'native-base';
+import antDesign from '../icon-list/AntDesign';
 import entypo from '../icon-list/Entypo';
 import evilIcons from '../icon-list/EvilIcons';
 import feather from '../icon-list/Feather';
 import fontAwesome from '../icon-list/FontAwesome';
+import fontAwesome5 from '../icon-list/FontAwesome5';
 import foundation from '../icon-list/Foundation';
 import ionicons from '../icon-list/Ionicons';
 import materialCommunityIcons from '../icon-list/MaterialCommunityIcons';
@@ -16,87 +18,89 @@ import zocial from '../icon-list/Zocial';
 
 export default class IconList extends Component {
 
-    state = {
-        iconFamily: undefined,
-        loading: true
+  state = {
+    iconFamily: undefined,
+    loading: true
+  }
+
+  // async componentWillMount() {
+  //     await Expo.Font.loadAsync({
+  //         Roboto: require("native-base/Fonts/Roboto.ttf"),
+  //         Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+  //         Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+  //     });
+  //     this.setState({ loading: false });
+  // }
+
+  componentDidMount() {
+    const { params } = this.props.navigation.state;
+    let iconFamily = params.iconFamily;
+    this.setState({ iconFamily })
+  }
+
+
+  getIconList(iconFamily) {
+    switch (iconFamily) {
+      case "AntDesign": return antDesign;
+      case "Entypo": return entypo;
+      case "EvilIcons": return evilIcons;
+      case "Feather": return feather;
+      case "FontAwesome": return fontAwesome;
+      case "FontAwesome5": return fontAwesome5;
+      case "Foundation": return foundation;
+      case "Ionicons": return ionicons;
+      case "MaterialCommunityIcons": return materialCommunityIcons;
+      case "MaterialIcons": return materialIcons;
+      case "Octicons": return octicons;
+      case "SimpleLineIcons": return simpleLineIcons;
+      case "Zocial": return zocial;
+      default: return ionicons;
     }
+  }
 
-    async componentWillMount() {
-        await Expo.Font.loadAsync({
-            Roboto: require("native-base/Fonts/Roboto.ttf"),
-            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-            Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-        });
-        this.setState({ loading: false });
-    }
+  render() {
+    const { params } = this.props.navigation.state;
+    let iconFamily = params.iconFamily;
+    let iconList = this.getIconList(iconFamily)
 
-    componentDidMount() {
-        const { params } = this.props.navigation.state;
-        let iconFamily = params.iconFamily;
-        this.setState({ iconFamily })
-    }
-
-
-    getIconList(iconFamily) {
-        switch (iconFamily) {
-            case "Entypo": return entypo;
-            case "EvilIcons": return evilIcons;
-            case "Feather": return feather;
-            case "FontAwesome": return fontAwesome;
-            case "Foundation": return foundation;
-            case "Ionicons": return ionicons;
-            case "MaterialCommunityIcons": return materialCommunityIcons;
-            case "MaterialIcons": return materialIcons;
-            case "Octicons": return octicons;
-            case "SimpleLineIcons": return simpleLineIcons;
-            case "Zocial": return zocial;
-            default: return ionicons;
-        }
-    }
-
-    render() {
-        const { params } = this.props.navigation.state;
-        let iconFamily = params.iconFamily;
-        let iconList = this.getIconList(iconFamily)
-
-        if (this.state.loading) {
-            return <Expo.AppLoading />;
-        }
-        return (
-            <Container>
-                <Header><Left>
-                    <Button transparent onPress={() => this.props.navigation.goBack()}>
-                        <Icon name='arrow-back' />
-                    </Button>
-                </Left>
-                    <Body>
-                        <Title>{this.state.iconFamily}</Title>
-                    </Body>
-                    <Right />
-                </Header>
-                <Content>
-                    <FlatList
-                        data={iconList}
-                        keyExtractor={(item, index) => String(index)}
-                        renderItem={({ item }) => <View style={styles.item}>
-                            <Text style={styles.text}>{item}</Text>
-                            <Icon name={item} type={iconFamily} />
-                        </View>}
-                    />
-                </Content>
-            </Container>
-        );
-    }
+    // if (this.state.loading) {
+    //     return <Expo.AppLoading />;
+    // }
+    return (
+      <Container>
+        <Header><Left>
+          <Button transparent onPress={() => this.props.navigation.goBack()}>
+            <Icon name='arrow-back' />
+          </Button>
+        </Left>
+          <Body>
+            <Title>{this.state.iconFamily}</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <FlatList
+            data={iconList}
+            keyExtractor={(item, index) => String(index)}
+            renderItem={({ item }) => <View style={styles.item}>
+              <Text style={styles.text}>{item}</Text>
+              <Icon name={item} type={iconFamily} />
+            </View>}
+          />
+        </Content>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    item: {
-        flexDirection: 'row',
-        padding: 5,
-        borderColor: 'grey',
-        borderBottomWidth: 1,
-        borderTopWidth: 1,
-        alignItems: 'center'
-    },
-    text: { marginRight: 15 }
+  item: {
+    flexDirection: 'row',
+    padding: 5,
+    borderColor: 'grey',
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    alignItems: 'center'
+  },
+  text: { marginRight: 15 }
 })
